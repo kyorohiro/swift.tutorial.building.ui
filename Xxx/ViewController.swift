3c53403d165fdafcbf6902d6cbce3f621228b724
kiyohiro kawamura <kyorohiro@gmail.com>
2018-12-21 08:31:28 +0900

    sc

--- Xxx/ViewController.swift
+++ Xxx/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
+
+    var textFieldDelegate:MealNameFieldDelegate? ;
+
+    // 新しく init を定義した場合に必須
+    required init?(coder aDecoder: NSCoder) {
+       // fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+        self.textFieldDelegate = MealNameFieldDelegate(self)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("called viewDidLoad")
+        mealNameField.delegate = self.textFieldDelegate
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        print("called didReceiveMemoryWarning")
+    }
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var mealNameField: UITextField!
+    @IBAction func onClickButton(_ sender: Any) {
+        mealNameLabel.text = "XXX"
+      //  mealNameField.delegate =
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
 
+class MealNameFieldDelegate : NSObject, UITextFieldDelegate {
+
+    var target:ViewController?
+
+    init(_ target:ViewController) {
+        self.target = target
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        print("called ")
+        textField.resignFirstResponder()
+        target?.mealNameLabel.text = textField.text
+        return true
+    }
 }
 
