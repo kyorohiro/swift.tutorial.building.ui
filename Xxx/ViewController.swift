94d01e7f35b3302b7abf6317cd7928d2ad25061c
潔広河村 <kyorohiro@gmail.com>
2018-12-20 19:16:41 +0900

    Initial Commit

---
+++ Xxx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xxx
+//
+//  Created by kiyohiro kawamura on 2018/12/20.
+//  Copyright © 2018 kiyohiro kawamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
