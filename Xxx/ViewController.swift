e076ab75aab8d9f6b869b0acdfd6ef539cdb839f
kiyohiro kawamura <kyorohiro@gmail.com>
2018-12-21 11:51:07 +0900

    th

--- Xxx/ViewController.swift
+++ Xxx/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController  {
+class ViewController: UIViewController,
+UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
 
     var textFieldDelegate:MealNameFieldDelegate? ;
 
@@ -17,12 +19,14 @@ class ViewController: UIViewController  {
        // fatalError("init(coder:) has not been implemented")
         super.init(coder: aDecoder)
         self.textFieldDelegate = MealNameFieldDelegate(self)
+        
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print("called viewDidLoad")
         mealNameField.delegate = self.textFieldDelegate
+       // photoImageView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,14 +38,47 @@ class ViewController: UIViewController  {
     
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var mealNameField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
+
     @IBAction func onClickButton(_ sender: Any) {
+        print("called Click Button")
         mealNameLabel.text = "XXX"
       //  mealNameField.delegate =
+
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
     }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
+    
+     //MARK: Actions
+    @IBAction func onTapPhoto(_ sender: UITapGestureRecognizer) {
+        print("called onTapPhoto")
+        self.mealNameField.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("called imagePickerControllerDidCancel")
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
     }
 }
 
